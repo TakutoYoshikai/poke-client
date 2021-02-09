5a91b8b0203e3c342a8f66a8c79868632ae185ad
lucky <takuto.yoshikai@gmail.com>
2021-02-09 18:06:37 +0900

    args

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const player = require("play-sound")();
+const request = require("request");
+
+const host = process.argv[2];
+const id = process.argv[3];
+
+function register(host, id) {
+  const options = {
+    url: "http://" + host + ":3000" + "/register/" + id,
+    method: "GET"
+  }
+  request(options, function(err, response, body) {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+register(host, id);
 
 app.get("/", (req, res) => {
   player.play("./nyanpass.mp3", err => {
     console.error(err);
   });
+  res.send({ message: "OK" });
 });
 
 app.listen(3002);
